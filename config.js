3c05316ca481e44e1eced5758b4265260dcab5f2
liqian1 <liqian1@supermap.com>
2019-05-15 18:32:09 +0800

    新增 props type 链接

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const env = require('jsdoc/env');
+const { resolve } = require('path');
 const getTemplatePath = require('./lib/core/getTemplatePath');
 
 const config = env.conf || {};
@@ -8,6 +9,7 @@ config['jsdoc-vuejs'] = config['jsdoc-vuejs'] || {};
 // Detect JSDoc template if not specified
 if (!Object.prototype.hasOwnProperty.call(config['jsdoc-vuejs'], 'template')) {
   config['jsdoc-vuejs'].template = getTemplatePath(env.opts.template || 'default',env.opts.configure);
+  env.opts.typeLinkExt && (env.opts.typeLinkExt = resolve(env.opts.configure,'../',env.opts.typeLinkExt));
 }
 
 module.exports = config;
